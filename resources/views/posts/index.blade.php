8a1fe5467fc8c0e831f2240d1d7f16e2078c41e8
Your Name <you@example.com>
2022-05-24 13:00:23 +0900

    score

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,15 +8,16 @@
 </head>
 <body>
     <h1>ハロー</h1>
+    {{Auth::user()->name}}
     <table>
         <thead>
             <tr>
                 <th>日付</th>
                 <th>タイトル</th>
+                <th>自己評価</th>
             </tr>
         </thead>
         <tbody>
-            {{Auth::user()->name}}
             <a href="{{route('posts.create')}}">新しい登録</a>
             @foreach($posts as $post)
             <tr>
@@ -27,6 +28,29 @@
                     </a>
                 </td>
                 <td>{{ $post->title }}</td>
+                <td>
+                    @switch($post->score)
+                        @case(0)
+                            <p>カス</p>
+                            @break
+                        @case(1)
+                            <p>ゴミ</p>
+                            @break
+                        @case(2)
+                            <p>まあまあ</p>
+                            @break
+                        @case(3)
+                            <p>ぼちぼち</p>
+                            @break
+                        @case(4)
+                            <p>やるやん</p>
+                            @break
+                        @case(5)
+                            <p>グレート</p>
+                            @break
+                    @endswitch
+
+                </td>
             </tr>
             @endforeach
         </tbody>
